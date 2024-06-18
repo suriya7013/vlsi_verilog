6ba7b61e526ab1cb7764d57f0fa443070adcc005
suriya7013 <55856510+suriya7013@users.noreply.github.com>
2024-06-18 14:43:24 +0530

    Create fulladder.v

---
+++ fulladder.v
@@ -0,0 +1,24 @@
+module full_adder(input a, b, cin, output S, Cout);
+  assign S = a ^ b ^ cin;
+  assign Cout = (a & b) | (b & cin) | (a & cin);
+endmodule
+
+// test bench.
+module tb_top;
+  reg a, b, c;
+  wire s, c_out;
+  
+  full_adder fa(a, b, c, s, c_out);
+  
+  initial begin
+    $monitor("At time %0t: a=%b b=%b, cin=%b, sum=%b, carry=%b",$time, a,b,c,s,c_out);
+    a = 0; b = 0; c = 0; #1;
+    a = 0; b = 0; c = 1; #1;
+    a = 0; b = 1; c = 0; #1;
+    a = 0; b = 1; c = 1; #1;
+    a = 1; b = 0; c = 0; #1;
+    a = 1; b = 0; c = 1; #1;
+    a = 1; b = 1; c = 0; #1;
+    a = 1; b = 1; c = 1;
+  end
+endmodule
