58d0366152929bd07b0faa327e8ec0dae7dc015b
suriya7013 <55856510+suriya7013@users.noreply.github.com>
2024-06-18 14:42:12 +0530

    Create halfadder.v

---
+++ halfadder.v
@@ -0,0 +1,23 @@
+module half_adder(input a, b, output s, Cout);
+  assign S = a ^ b;
+  assign Cout = a & b;
+endmodule
+
+//test bench
+module tb_top;
+  reg a, b;
+  wire s, c_out;
+  
+  half_adder ha(a, b, s, c_out);
+  
+  initial begin
+    $monitor("At time %0t: a=%b b=%b, sum=%b, carry=%b",$time, a,b,s,c_out);
+    a = 0; b = 0;
+    #1;
+    a = 0; b = 1;
+    #1;
+    a = 1; b = 0;
+    #1;
+    a = 1; b = 1;
+  end
+endmodule
